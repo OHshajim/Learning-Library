7b2c770fdc12674924fca2a285aa8b0d516a3263
OHshajim <ajshajimmax@gmail.com>
2024-03-15 00:28:28 +0600

    books mark complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Carts from './assets/components/carts/Carts'
 
 function App() {
 
   return (
     <>
 
-      <h1 className='text-7xl'>Vite + React</h1>
+      <h1 className='text-5xl text-center mt-10'>Learning Store</h1>
+      <h3 className='text-xl text-center mt-3 text-[#ff4747]'> learn with peace mind </h3>
+      <Carts></Carts>
 
     </>
   )
---
+++ src/assets/components/Books/Book.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Book = ({book,handlePurchase}) => {
+    const {author,bookTitle,img,MPrice,rating} = book
+   
+    return (
+        <div className='border-2 border-[#ff4747] p-5 m-6 w-80 rounded-xl'>
+            <img src={img} alt="" className='w-full h-64 rounded-xl' />
+            <h3 className='text-lg font-semibold mt-2'> {bookTitle}</h3>
+            <h3 className='text-lg mt-5'> {author}</h3>
+
+            <p className='mt-1 text-base '>Review : {rating?rating:"(00)"}</p>
+            <div className='flex  justify-between items-center mt-2 font-bold'>
+            <h4 className='text-lg'>Price : {MPrice}</h4>
+            <button className='px-5 text-lg  py-2 bg-[#ff4747] text-white' onClick={()=>handlePurchase(bookTitle)}>Purchase</button>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
