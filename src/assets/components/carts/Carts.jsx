5902829c127d58fa0a9c9ab177c6b4b9cca91f4d
OHshajim <ajshajimmax@gmail.com>
2024-03-15 00:35:11 +0600

    responsive done

--- src/assets/components/carts/Carts.jsx
+++ src/assets/components/carts/Carts.jsx
@@ -24,25 +24,26 @@ const Carts = () => {
     return (
         <>
             <h3 className="text-2xl mx-5  p-2 ">Books : {carts.length}</h3>
-            <div className=" flex m-5 justify-between  ">
-                <div className="flex-2">
-
-                    <div className="grid grid-cols-1  sm:grid-cols-2 xl:grid-cols-3">
-                        {carts.map(book => <Book key={book.bookTitle} book={book} handlePurchase={handlePurchase}></Book>)}
-                    </div>
-                </div>
+            <div className=" flex xl:flex-row flex-col m-5 justify-between  ">
 
                 <div className="m-5 flex-1">
                     <div className="bg-[#574848b6]  rounded-xl p-10  ">
                         <h3 className=" py-2 px-3 rounded-xl ">Mark : <span className="text-red-500 font-bold"> {Books.length}</span></h3><hr />
                  
-                 {Books.map(book=><div key={book.bookTitle} className="my-5 border px-5 py-3 rounded-xl flex  items-center justify-between">
+                 {Books.map(book=><div key={book.bookTitle} className="my-5 border px-5 py-3 rounded-xl  flex   items-center justify-between">
                             <div>
-                            <h3 className="text-xl font-semibold ">{book.bookTitle}</h3>
+                            <h3 className="sm:text-xl text-base font-semibold ">{book.bookTitle}</h3>
                             <h4 className="text-sm mt-2">{book.author}</h4>
                             </div>
-                            <h3 className="text-xl font-semibold text-[#ff4747]">Price : {book.MPrice}</h3>
+                            <h3 className="sm:text-xl text-base  font-semibold text-[#ff4747]">Price : {book.MPrice}</h3>
                         </div>)}
+                    </div>
+                </div>
+
+                <div className="flex-2">
+
+                    <div className="grid grid-cols-1  sm:grid-cols-2 xl:grid-cols-3">
+                        {carts.map(book => <Book key={book.bookTitle} book={book} handlePurchase={handlePurchase}></Book>)}
                     </div>
                 </div>
             </div>
